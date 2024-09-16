87660b6e8b4618dcec088e6e802198fb693391f2
galvao.dev <inosuke567765@gmail.com>
2024-09-16 09:46:03 -0300

    feat: adição do middleware que fará o controle de acesso a algumas rotas permicionadas e alteração da uri de redirect do middleware nativo auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.if.admin' => \App\Http\Middleware\IsAdmin::class,
         'email.check.validation' => \App\Http\Middleware\CheckIfEmailItsValidated::class
     ];
 }
---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        // Debugging
+        dd($user, $user->isAdmin());
+
+        if ($user && $user->isAdmin()) {
+            return $next($request);
+        }
+
+        return redirect()->back()->with('error', 'Você não tem permissão para acessar esta página.');
+    }
+}
