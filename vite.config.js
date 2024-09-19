858be2bcf1db2d7f6892b73be0a04052919b5cb7
galvao.dev <inosuke567765@gmail.com>
2024-09-19 11:11:17 -0300

    wip: trabalhando na lógica responsável pelo fiveicon do site

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/favicon_package/apple-touch-icon.png",
+                "resources/favicon_package/favicon-32x32.png",
+                "resources/favicon_package/favicon-16x16.png",
+                "resources/favicon_package/favicon.ico",
+                "resources/favicon_package/site.webmanifest",
+                "resources/favicon_package/safari-pinned-tab.svg",
+                "resources/favicon_package/android-chrome-192x192.png",
+                "resources/favicon_package/android-chrome-384x384.png",
+                "resources/favicon_package/mstile-150x150.png",
+                "resources/favicon_package/browserconfig.xml",
+            ],
             refresh: true,
         }),
     ],
