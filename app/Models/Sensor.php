a610b3237b5d087135c492d8574d24d2b193ff55
galvao.dev <inosuke567765@gmail.com>
2024-10-11 12:14:34 -0300

    refactor: renomeando o model Sensor para se adequar aos padrões do laravel

--- app/Models/Sensor.php
+++ app/Models/Sensor.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sensors extends Model
+class Sensor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'percent_mq3',
+        'percent_mq5'
+    ];
 }
