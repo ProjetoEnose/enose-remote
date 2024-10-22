becdffee85e539b42a2b73c17ef6959a6abcf92e
galvao.dev <inosuke567765@gmail.com>
2024-10-22 12:14:12 -0300

    wip: pequenas alterações de estilo e estruturais

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,9 +1,7 @@
 import { addEventListenerInBtnAlterMenu } from "./sidebar.js";
 import { addEventListenerInBtnAlterMenuMobile } from "./header.js";
-import { addListenerInBntsEditionUserSettings } from "./user-settings.js";
 
 window.onload = () => {
-  addEventListenerInBtnAlterMenu();
-  addEventListenerInBtnAlterMenuMobile();
-  addListenerInBntsEditionUserSettings();
+    addEventListenerInBtnAlterMenu();
+    addEventListenerInBtnAlterMenuMobile();
 };
